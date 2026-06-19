96460b67246d0c6d59f4649edaff1daa1a3430cd
bcherishma <bcherishma@gmail.com>
2026-06-19 17:16:24 +0530

    minor change

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 import streamlit as st
 import yaml
 from frontend.components.sidebar import render_sidebar
- 
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
